cc8946f10ab2434a775ea4f37f183fb7729eec32
Qais Patankar <qaisjp@gmail.com>
2019-09-14 20:03:35 -0400

    change version_test.go package to ver_test

--- internal/ver/version_test.go
+++ internal/ver/version_test.go
@@ -1,23 +1,24 @@
-package ver
+package ver_test
 
 import (
 	"testing"
 
+	"github.com/multitheftauto/build-tools/internal/ver"
 	"github.com/stretchr/testify/assert"
 )
 
-func performTest(t *testing.T, verstr string, v MtaVersion) {
-	assert.Equal(t, MtaVersionFromString(verstr), v)
+func performTest(t *testing.T, verstr string, v ver.MtaVersion) {
+	assert.Equal(t, ver.MtaVersionFromString(verstr), v)
 }
 
 func TestFull(t *testing.T) {
-	performTest(t, "1.3.2-9.03931", MtaVersion{1, 3, 2, 3931})
+	performTest(t, "1.3.2-9.03931", ver.MtaVersion{1, 3, 2, 3931})
 }
 
 func TestToPatch(t *testing.T) {
-	performTest(t, "1.3.1", MtaVersion{1, 3, 1, 0})
+	performTest(t, "1.3.1", ver.MtaVersion{1, 3, 1, 0})
 }
 
 func TestToMinor(t *testing.T) {
-	performTest(t, "1.3", MtaVersion{1, 3, 0, 0})
+	performTest(t, "1.3", ver.MtaVersion{1, 3, 0, 0})
 }
