0145daf71af74d74452ee11487813b06e5649b04
Qais Patankar <qaisjp@gmail.com>
2019-09-14 20:09:09 -0400

    complete ReadVersionHeader

--- internal/ver/file.go
+++ internal/ver/file.go
@@ -2,23 +2,38 @@ package ver
 
 import (
 	"bufio"
-	"fmt"
 	"io"
+	"strconv"
 	"strings"
 )
 
-var versionPrefixes = []string{
+var versionDefines = []string{
 	"MTASA_VERSION_MAJOR", "MTASA_VERSION_MINOR", "MTASA_VERSION_MAINTENANCE",
 }
 
+// ReadVersionHeader reads a file in the form of version.h and extracts the MtaVersion struct
 func ReadVersionHeader(r io.Reader) (v MtaVersion, err error) {
 	scanner := bufio.NewScanner(r)
 
 	for scanner.Scan() {
 		text := scanner.Text()
-		for _, prefix := range versionPrefixes {
-			if strings.HasPrefix(text, "#define "+prefix) {
-				fmt.Println(text)
+		for _, def := range versionDefines {
+			prefix := "#define " + def
+			if strings.HasPrefix(text, prefix) {
+				text = strings.TrimSpace(strings.TrimPrefix(text, prefix))
+				n, err := strconv.Atoi(text)
+				if err != nil {
+					return v, err
+				}
+
+				switch def {
+				case "MTASA_VERSION_MAJOR":
+					v.Major = n
+				case "MTASA_VERSION_MINOR":
+					v.Minor = n
+				case "MTASA_VERSION_MAINTENANCE":
+					v.Patch = n
+				}
 			}
 		}
 	}
@@ -26,5 +41,6 @@ func ReadVersionHeader(r io.Reader) (v MtaVersion, err error) {
 	if err := scanner.Err(); err != nil {
 		return v, err
 	}
+
 	return v, nil
 }
