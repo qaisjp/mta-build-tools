e0b94f960c52173077090b3ce0ac576c4dcff09d
Qais Patankar <qaisjp@gmail.com>
2019-09-14 20:16:08 -0400

    add MtaVersion.String()

--- internal/ver/version.go
+++ internal/ver/version.go
@@ -1,11 +1,15 @@
 package ver
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
 
-// This might
+// MtaVersion is a very primitive representation of an instance of an MTA version.
+//
+// - It assumes that the build type is "9" (release).
+// - It ignores "build revision" (this is not the same as r9000 revisions)
 type MtaVersion struct {
 	Major       int
 	Minor       int
@@ -13,6 +17,14 @@ type MtaVersion struct {
 	BuildNumber int
 }
 
+func (v MtaVersion) String() string {
+	if v.Major > 9 || v.Minor > 9 || v.Patch > 9 {
+		panic("version major, minor or patch are too large to represent as a sortable string")
+	}
+	return fmt.Sprintf("%d.%d.%d-9.%05d", v.Major, v.Minor, v.Patch, v.BuildNumber)
+}
+
+// MtaVersionFromString parses MTA sortable version strings
 func MtaVersionFromString(str string) (v MtaVersion) {
 	parts := strings.Split(str, "-")
 
